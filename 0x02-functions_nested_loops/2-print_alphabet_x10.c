25799e39107b7daec6673ca06849c4e5a9c33646
Mubzy2 <mubarakabdulrasheed30@gmail.com>
2022-10-19 21:57:02 -0700

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - repeats the print_alphabet 10 times.
+ *  Return: Nothing.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; ++i)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
